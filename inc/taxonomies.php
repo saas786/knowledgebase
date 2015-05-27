7e4eba39e9d2d91ba286cc4005fd097e766500b0
syed <syed.sayal.shah@gmail.com>
2015-05-27 09:52:28 +0500

    changed category slug to for

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -39,7 +39,7 @@ function knowledgebase_register_taxonomies() {
 
 		/* The rewrite handles the URL structure. */
 		'rewrite' => array(
-			'slug'         => kbp_knowledgebase_base() . '/category',
+			'slug'         => kbp_knowledgebase_base() . '/for',
 			'with_front'   => false,
 			'hierarchical' => true,
 			'ep_mask'      => EP_NONE
